21403645ef2fd1969a69d748ec435599712b6543
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2019-02-20 13:48:50 +0700

    Add DeepDriveDataset class in init file

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -2,5 +2,6 @@
 from .coco import COCODataset
 from .voc import PascalVOCDataset
 from .concat_dataset import ConcatDataset
+from .deepdrive import DeepDriveDataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
+__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset", "DeepDriveDataset"]
