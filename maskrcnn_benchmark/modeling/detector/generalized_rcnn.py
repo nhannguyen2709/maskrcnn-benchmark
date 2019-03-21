580be7bd0fe84d0e42cea88db176bda497fdf477
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2019-03-22 03:03:26 +0900

    Add module consisting of both student and teacher networks for distillation train

--- maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
+++ maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
@@ -9,6 +9,7 @@ from torch import nn
 from maskrcnn_benchmark.structures.image_list import to_image_list
 
 from ..backbone import build_backbone
+from ..backbone import build_teacher_backbone
 from ..rpn.rpn import build_rpn
 from ..roi_heads.roi_heads import build_roi_heads
 
@@ -63,3 +64,76 @@ class GeneralizedRCNN(nn.Module):
             return losses
 
         return result
+
+
+class GeneralizedRCNNDistil(nn.Module):
+    """
+    Main class for Generalized R-CNN. Currently supports boxes and masks.
+    It consists of three main parts:
+    - backbone
+    - rpn
+    - heads: takes the features + the proposals from the RPN and computes
+        detections / masks from it.
+    """
+
+    def __init__(self, cfg):
+        super(GeneralizedRCNNDistil, self).__init__()
+
+        self.backbone = build_backbone(cfg)
+        self.rpn = build_rpn(cfg, self.backbone.out_channels)
+        self.roi_heads = build_roi_heads(cfg, self.backbone.out_channels)
+
+        cfg_clone = cfg.clone()
+        cfg_clone.MODEL.RESNETS.BACKBONE_OUT_CHANNELS = 256
+        cfg_clone.MODEL.RESNETS.STRIDE_IN_1X1 = False
+        cfg_clone.MODEL.RESNETS.NUM_GROUPS = 32
+        cfg_clone.MODEL.RESNETS.WIDTH_PER_GROUP = 8
+        self.teacher_backbone = build_teacher_backbone(cfg_clone)
+        self.teacher_rpn = build_rpn(cfg_clone, self.teacher_backbone.out_channels)
+
+        for p in self.teacher_backbone.parameters():
+            p.requires_grad = False
+        for p in self.teacher_rpn.parameters():
+            p.requires_grad = False
+
+    def forward(self, images, targets=None):
+        """
+
+        Arguments:
+            images (list[Tensor] or ImageList): images to be processed
+            targets (list[BoxList]): ground-truth boxes present in the image (optional)
+
+        Returns:
+            result (list[BoxList] or dict[Tensor]): the output from the model.
+                During training, it returns a dict[Tensor] which contains the losses.
+                During testing, it returns list[BoxList] contains additional fields
+                like `scores`, `labels` and `mask` (for Mask R-CNN models).
+
+        """
+        if self.training and targets is None:
+            raise ValueError("In training mode, targets should be passed")
+        images = to_image_list(images)
+        features = self.backbone(images.tensors)
+
+        self.teacher_backbone.eval()
+        self.teacher_rpn.eval()
+        teacher_features = self.teacher_backbone(images.tensors)
+        teacher_box_cls, teacher_box_regression = self.teacher_rpn.head(
+            teacher_features)
+
+        proposals, proposal_losses = self.rpn(
+            images, features, teacher_box_cls, teacher_box_regression, targets)
+        if self.roi_heads:
+            x, result, detector_losses = self.roi_heads(
+                features, proposals, targets)
+        else:
+            # RPN-only models don't have roi_heads
+            x = features
+            result = proposals
+            detector_losses = {}
+
+        if self.training:
+            losses = {}
+            losses.update(detector_losses)
+            losses.update(proposal_losses)
+            return losses
